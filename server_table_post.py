7a3386dab1737d71d724efad94bdf73bdced1935
Yun-Ha Shin <yshin@triumf.ca>
2022-04-22 08:48:20 -0700

    added hidden 'id' column, and made 'age' visible again

--- server_table_post.py
+++ server_table_post.py
@@ -18,6 +18,7 @@ class User(db.Model):
     def to_dict(self):
         return {
             'DT_RowId': f'row_{self.id}',
+            'id': self.id,
             'name': self.name,
             'age': self.age,
             'address': self.address,
